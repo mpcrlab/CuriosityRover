ba00749a88294ed96c6b5b0e0808e8f900c61c32
michael teti <mtetifau@gmail.com>
2017-02-21 21:33:04 -0500

    cleaned up a little and added some comments

--- LCADictionary.py
+++ LCADictionary.py
@@ -1,16 +1,15 @@
-import tensorflow as tf
 import numpy as np
 from skimage.util import view_as_windows
 
 class LCADictionary():
 	def __init__(self):
-		self.patch_size = 64 * 3 # patch size 8x8x3
-		self.num_dict_patches = 200 # number of dictionary patches
+		self.ps = np.array([8, 8, 3]) # patch size 8x8x3
+		self.num_dict_patches = 200  # number of dictionary patches
 		self.threshhold = 0.1
-		self.ps = np.sqrt(self.patch_size / 3)
-		self.alpha = None
-		# Initializing random dictionary
-		self.LCADict = np.random.rand(self.patch_size, self.num_dict_patches)
+		self.alpha = None  # initialize sparse coefficients 
+
+		# Initializing dictionary with random values
+		self.LCADict = np.random.rand(self.ps.size, self.num_dict_patches)
 
 
 	# Updates dictionary with one 
@@ -18,8 +17,11 @@ class LCADictionary():
 
 	# Passed in 120,320,3 image
 	def update(self, image):
-		patches = view_as_windows(image, (self.ps, self.ps, 3))
 
+		# get 8x8x3 patches from the video frame
+		patches = view_as_windows(image, (self.ps[0], self.ps[1], self.ps[2]))
+
+		#vectorize patches 
 		patches=patches.reshape([patches.shape[0]* 
 								patches.shape[1]*
 								patches.shape[2], -1])
@@ -29,26 +31,30 @@ class LCADictionary():
 		patches = (patches - np.mean(patches)) / (np.std(patches, axis=0) + 1e-6)
 
 		# Scales dictionary between 0 -> 1 from 0 -> 255
-		self.LCADict = np.matmul(self.LCADict, np.diag(1/tf.sqrt(np.sum(self.LCADict**2, 0))))
+		self.LCADict = np.matmul(self.LCADict, np.diag(1/np.sqrt(np.sum(self.LCADict**2, 0))))
 
 		# Get sparse coeficients of dictionary to recover patches 
-		self.alpha = np.matmul(tf.transpose(self.LCADict), patches)
+		self.alpha = np.matmul(np.transpose(self.LCADict), patches)
 
-		# Scales dictionary between 0 -> 1
+		# Normalizes dictionary between 0 -> 1
 		self.alpha = np.matmul(self.alpha, np.diag(1/np.sqrt(np.reduce_sum(self.alpha**2, 0))))
 
 		# Cubic sparsity function
 		self.alpha=0.3 * self.alpha**3
 
-		# Updates dictionary
+		# Dictionary update step
 		self.LCADict = self.LCADict+np.matmul((patches-np.matmul(self.LCADict, self.alpha)), np.transpose(self.alpha))
 
 
 	def isImageRepresented(self, image):
+		''' Determines if the objects in the frame are represented well in the short-term dictionary by
+			taking the l1-norm of each sparse coefficient vector. The one with the lowest activations 
+			is not represented well by the dictionary, and is a novel object. '''
+
 		if self.alpha is None:
 			return False
         
-		# Get sum along zero axis in LCADict
+		# Get l1-norm of each sparse coefficient vector
 		l1_norm = np.sum(self.alpha, axis=0)
 
 		#get minimum l1 norm index
